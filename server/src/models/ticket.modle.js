c0951a7234e36b366b1ff50a6cd78841785b05ab
fardeen077 <fardeen36143@gmail.com>
2026-01-23 18:02:48 +0530

    refactor ticket and user models for improved schema structure and consistency

--- server/src/models/ticket.modle.js
+++ server/src/models/ticket.modle.js
@@ -1,6 +1,36 @@
 import mongoose from "mongoose";
 
 const ticketSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    assignedTo: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        default: null,
+    },
+    priority: {
+        type: String,
+        enum: ["LOW", "MEDIUM", "HIGH"],
+        default: "LOW",
+    },
+    status: {
+        type: String,
+        enum: ["OPEN", "IN_PROGRESS", "CLOSED"],
+        default: "OPEN",
+    },
 
 }, { timestamps: true });
 
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,32 +1,36 @@
 import mongoose from "mongoose";
 
-const userSchema =  new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
         trim: true,
     },
     email: {
-        type:String,
-        unique:true,
-        required:true,
-        lowercase:true,
-        trim:true,
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
         minlength: 6,
+        select: true,
     },
     role: {
-        type:String,
-        enum:["USER", "ADMIN", "AGENT"],
-        default:"USER"
+        type: String,
+        enum: ["USER", "ADMIN", "AGENT"],
+        default: "USER"
     },
-    isActive:{
-        type:Boolean,
+    isActive: {
+        type: Boolean,
         default: true
     },
-}, {timestamps: true});
+    refreshToken: {
+        type: String
+    }
+}, { timestamps: true });
 
 export const User = mongoose.model("User", userSchema);
