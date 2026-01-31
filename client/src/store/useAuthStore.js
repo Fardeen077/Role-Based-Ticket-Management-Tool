e0299881e34275639735a57e67267d64464e0fe0
fardeen077 <fardeen36143@gmail.com>
2026-01-31 11:14:26 +0530

    feat: implement authentication store with register, login, getUser, and logout functions

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,61 @@
+import {
+    registerApi,
+    loginApi,
+    logoutApi,
+    getUserApi
+} from "../api/authApi";
+import create from "zustand";
+
+const useAuthStore = create((set) => ({
+    isAuth: false,
+    isLoadind: false,
+    authUser: null,
+    error: null,
+
+    register: async (userData) => {
+        set({ isLoadind: true, error: null });
+        try {
+            const res = await registerApi(userData);
+            set({ isLoadind: false, authUser: res.data.user, isAuth: true })
+            return res.data;
+        } catch (error) {
+            set({ isLoadind: false, error: error?.response?.data?.message || "Register failed" })
+        }
+    },
+
+
+    login: async (userData) => {
+        set({ isLoadind: true, error: null });
+        try {
+            const res = await loginApi(userData);
+            set({ isLoadind: false, authUser: res.data.user, isAuth: true })
+            return res.data;
+        } catch (error) {
+            set({ isLoadind: false, error: error?.response?.data?.message || "Login failed" })
+        }
+    },
+
+    getUser: async () => {
+        set({ isLoadind: true, error: null });
+        try {
+            const res = await getUserApi();
+            set({ isLoadind: false, authUser: res.data, isAuth: true })
+            return res.data;
+        } catch (error) {
+            set({ isLoadind: false, error: error?.response?.data?.message || "Get User failed" })
+        }
+    },
+
+    logout: async () => {
+        set({ isLoadind: true, error: null });
+        try {
+            const res = await logoutApi();
+            set({ isLoadind: false, authUser: null, isAuth: false })
+            return res.data;
+        } catch (error) {
+            set({ isLoadind: false, error: error?.response?.data?.message || "Logout failed" })
+        }
+    },
+}));
+
+export default useAuthStore;
