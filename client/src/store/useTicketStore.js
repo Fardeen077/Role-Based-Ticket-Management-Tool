1eb72c91e01bc3d567c59361359d935180983112
fardeen077 <fardeen36143@gmail.com>
2026-01-31 12:55:57 +0530

    feat: implement ticket management store with create, update, assign, search, and fetch functions

---
+++ client/src/store/useTicketStore.js
@@ -0,0 +1,88 @@
+import {
+    createTicketApi,
+    updateStatusApi,
+    assignedTicketApi,
+    getTicketApi,
+    searchUserApi,
+} from "../api/ticketsApi"
+import create from "zustand";
+
+const initialState = {
+    tickets: [],
+    count: 0,
+    isLoading: false,
+    error: null,
+};
+const useTicketStore = create((set) => ({
+    ...initialState,
+
+    createTicket: async (ticket) => {
+        set({ isLoading: true, error: null });
+        try {
+            const res = await createTicketApi(ticket);
+            set((state) => ({
+                isLoading: false,
+                tickets: [res.data, ...state.data,]
+            }));
+        } catch (error) {
+            set({ isLoading: false, error: error?.response?.data?.message || "Create ticket failed" });
+        }
+    },
+
+    updateStatus: async (id) => {
+        set({ isLoading: true, error: null });
+        try {
+            await updateStatusApi(id);
+            set((state) => ({
+                isLoading: false,
+                tickets: state.tickets.map((stateus) => stateus._id !== id)
+            }));
+        } catch (error) {
+            set({ isLoading: false, error: error?.response?.data?.message || "Update status falied" })
+        }
+    },
+
+    assignedTicket: async (ticketId) => {
+        set({ isLoading: true, error: null });
+        try {
+            const res = await assignedTicketApi(ticketId);
+            set((state) => ({
+                tickets: state.tickets.map((ticket) => ticket._id === ticketId ? res.data : ticket)
+            }));
+        } catch (error) {
+            set({
+                isLoading: false,
+                error: error?.response?.data?.message || "Assign failed",
+            })
+        }
+    },
+
+    searchUser: async (query) => {
+        set({ isLoading: true, error: null });
+        try {
+            const res = await searchUserApi(query);
+            set({ isLoading: false, tickets: res.data });
+            return res.data;
+        } catch (error) {
+            set({ isLoading: false, error: error?.response?.data?.message || "Not found" })
+        }
+    },
+
+    getTicket: async (filters = {}) => {
+        set({ isLoading: true, error: null });
+        try {
+            const res = await getTicketApi(filters);
+            set({
+                isLoading: true,
+                tickets: res.tickets,
+                count: res.length,
+            });
+            return res.data
+        } catch (error) {
+            set({ isLoading: false, error: error?.response?.data?.message || "Failed to fetch tickets" })
+        }
+    },
+    resetTickets: () => set(initialState),
+}));
+
+export default useTicketStore;
