551dda193b283167ed5bfb4262e9d8719c14d756
fardeen077 <fardeen36143@gmail.com>
2026-02-02 22:10:14 +0530

    feat: add ProtectedRoute and PublicRoute components for route protection based on authentication status

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import {Navigate} from "react-router-dom"
+import useAuthStore from "../store/useAuthStore"
+
+function ProtectedRoute({children}) {
+    const isAuth = useAuthStore((state) => state.isAuth);
+    if(!isAuth) return <Navigate to="login" replace/>
+    return children;
+}
+
+export default ProtectedRoute
